e7fc0cb1cf869561637856a5739a825847c8b8f7
Luca Maria Incarnato <lucaincarnato.03@gmail.com>
2025-06-11 11:12:25 +0200

    Initial Commit

---
+++ AttentionQuack/AttentionQuackApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttentionQuackApp.swift
+//  AttentionQuack
+//
+//  Created by Luca Maria Incarnato on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AttentionQuackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
